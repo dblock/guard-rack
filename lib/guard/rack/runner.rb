a6e4737639b0d010252dbc2b5ff0b99853f55c14
Jeremy Stephens <viking@pillageandplunder.net>
2013-01-14 15:54:17 -0600

    Use Ruby 1.9 libraries to spawn rackup

--- lib/guard/rack/runner.rb
+++ lib/guard/rack/runner.rb
@@ -1,35 +1,43 @@
 require 'fileutils'
+require 'timeout'
 
 module Guard
   class RackRunner
 
-    MAX_WAIT_COUNT = 10
-
-    attr_reader :options
+    attr_reader :options, :pid
 
     def initialize(options)
       @options = options
     end
 
-    def kill pid
-      system %{kill -INT #{pid}}
-      $?.exitstatus
+    def kill
+      result = -1
+
+      Process.kill("INT", @pid)
+      begin
+        Timeout.timeout(options[:timeout]) do
+          _, status = Process.wait2(@pid)
+          result = status.exitstatus
+        end
+      rescue Timeout::Error
+        Process.kill(9, @pid)
+      end
+      @pid = nil
+
+      result
     end
 
     def start
       kill_unmanaged_pid! if options[:force_run]
-      run_rack_command!
-      wait_for_pid
+      @pid = run_rack_command!
+      true
     end
 
     def stop
-      # Rely on kill_unmanaged_pid if there's no pid file
-      return true unless File.file?(pid_file)
- 
-      if kill(pid) == 0
-        wait_for_no_pid
-        remove_pid_file
-      else
+      # Rely on kill_unmanaged_pid if there's no pid
+      return true unless @pid
+
+      if kill != 0
         UI.info "Rackup exited with non-zero exit status whilst trying to stop."
         return false
       end
@@ -43,54 +51,27 @@ module Guard
 
     def build_rack_command
       rack_options = [
-				options[:config],
+        options[:config],
         '--env', options[:environment],
-        '--port', options[:port],
-        '--pid', pid_file
+        '--port', options[:port]
       ]
 
       rack_options << '--daemonize' if options[:daemon]
       rack_options << '--debug' if options[:debugger]
       rack_options << ["--server",options[:server]] if options[:server]
 
-      %{sh -c 'cd #{Dir.pwd} && rackup #{rack_options.join(' ')} &'}
-    end
-
-    def pid_file
-      File.expand_path(".guard-rack-#{options[:environment]}.pid")
-    end
-
-    def pid
-      File.file?(pid_file) ? File.read(pid_file).to_i : nil
-    end
-
-    def remove_pid_file
-      FileUtils.rm pid_file if File.exist? pid_file
-    end
-
-    def sleep_time
-      options[:timeout].to_f / MAX_WAIT_COUNT.to_f
+      %{cd #{Dir.pwd} && rackup #{rack_options.join(' ')}}
     end
 
     private
-    
+
     def run_rack_command!
-      system build_rack_command
-    end
-
-    def has_pid?
-      File.file?(pid_file)
-    end
-
-    def wait_for_pid_action
-      sleep sleep_time
+      Process.spawn(build_rack_command)
     end
 
     def kill_unmanaged_pid!
       if pid = unmanaged_pid
         kill pid
-        wait_for_no_pid
-        remove_pid_file
       end
     end
 
@@ -102,25 +83,5 @@ module Guard
       }
       nil
     end
-
-    private
-
-      def wait_for_pid
-        wait_for_pid_loop
-      end
-
-      def wait_for_no_pid
-        wait_for_pid_loop(false)
-      end
-
-      def wait_for_pid_loop(check_for_existince = true)
-        count = 0
-        while !(check_for_existince ? has_pid? : !has_pid?) && count < MAX_WAIT_COUNT
-          wait_for_pid_action
-          count += 1
-        end
-        !(count == MAX_WAIT_COUNT)
-      end
-      
   end
 end
--- spec/lib/guard/runner_spec.rb
+++ spec/lib/guard/runner_spec.rb
@@ -1,32 +1,29 @@
 require 'spec_helper'
 require 'guard/rack/runner'
-require 'fakefs/spec_helpers'
 
 describe Guard::RackRunner do
   let(:runner) { Guard::RackRunner.new(options) }
   let(:environment) { 'development' }
   let(:port) { 3000 }
-  
+
   let(:default_options) { { :environment => environment, :port => port, :config => 'config.ru' } }
   let(:options) { default_options }
-  
-  describe '#pid' do
-    include FakeFS::SpecHelpers
 
-    context 'pid file exists' do
-      let(:pid) { 12345 }
+  describe '#pid' do
+    context 'after running' do
+      let(:pid) { 1234 }
 
       before do
-        FileUtils.mkdir_p File.split(runner.pid_file).first
-        File.open(runner.pid_file, 'w') { |fh| fh.print pid }
+        Process.stubs(:spawn).returns(pid)
+        runner.start
       end
 
-      it "should read the pid" do
+      it "should not be nil" do
         runner.pid.should == pid
       end
     end
 
-    context 'pid file does not exist' do
+    context 'before running' do
       it "should return nil" do
         runner.pid.should be_nil
       end
@@ -47,7 +44,7 @@ describe Guard::RackRunner do
         runner.build_rack_command.should match(%r{ --daemonize})
       end
     end
-    
+
     context 'debugger' do
       let(:options) { default_options.merge(:debugger => true) }
 
@@ -55,6 +52,7 @@ describe Guard::RackRunner do
         runner.build_rack_command.should match(%r{ --debug})
       end
     end
+
     context "server" do
       let(:options) { default_options.merge(:server => "thin") }
 
@@ -63,35 +61,34 @@ describe Guard::RackRunner do
       end
     end
 
-		context "config file" do
-			context "default" do
-				it "should default to config.ru" do
-					runner.build_rack_command.should match(%r{ config.ru})
-				end
-			end
-			context "custom" do
-				let(:options) { default_options.merge(:config => 'config2.ru') }
-				it "should honour config option" do
-					options = default_options.merge(:config => 'config2.ru')
-					runner.build_rack_command.should match(%r{ config2.ru})
-				end
-			end
-		end
+    context "config file" do
+      context "default" do
+        it "should default to config.ru" do
+          runner.build_rack_command.should match(%r{ config.ru})
+        end
+      end
+
+      context "custom" do
+        let(:options) { default_options.merge(:config => 'config2.ru') }
+        it "should honour config option" do
+          options = default_options.merge(:config => 'config2.ru')
+          runner.build_rack_command.should match(%r{ config2.ru})
+        end
+      end
+    end
   end
 
   describe '#start' do
     let(:kill_expectation) { runner.expects(:kill_unmanaged_pid!) }
-    let(:pid_stub) { runner.stubs(:has_pid?) }
+    let(:pid) { 1234 }
 
     before do
-      runner.expects(:run_rack_command!).once
+      Process.expects(:spawn).once.returns(pid)
     end
 
     context 'do not force run' do
       before do
-        pid_stub.returns(true)
         kill_expectation.never
-        runner.expects(:wait_for_pid_action).never
       end
 
       it "should act properly" do
@@ -103,47 +100,35 @@ describe Guard::RackRunner do
       let(:options) { default_options.merge(:force_run => true) }
 
       before do
-        pid_stub.returns(true)
         kill_expectation.once
-        runner.expects(:wait_for_pid_action).never
       end
 
       it "should act properly" do
         runner.start.should be_true
       end
     end
-
-    context "don't write the pid" do
-      before do
-        pid_stub.returns(false)
-        kill_expectation.never
-        runner.expects(:wait_for_pid_action).times(Guard::RackRunner::MAX_WAIT_COUNT)
-      end
-
-      it "should act properly" do
-        runner.start.should be_false
-      end
-    end
   end
 
   describe '#stop' do
-    
-    include FakeFS::SpecHelpers
 
-    context 'pid file exists' do
+    context 'pid exists' do
       let(:pid) { 12345 }
-      let(:kill_stub) { runner.stubs(:kill) }
-      let(:pid_stub) { runner.stubs(:has_pid?) }
+      let(:status_stub) { stub('process exit status') }
+      let(:wait_stub) { Process.stubs(:wait2) }
 
       before do
-        FileUtils.mkdir_p File.split(runner.pid_file).first
-        File.open(runner.pid_file, 'w') { |fh| fh.print pid }
+        Process.stubs(:spawn).returns(pid)
+        runner.start
+
+        Process.expects(:kill).with('INT', pid)
       end
 
       context 'rackup returns successful exit status' do
         before do
-          kill_stub.returns(0)
+          wait_stub.returns([pid, status_stub])
+          status_stub.stubs(:exitstatus).returns(0)
         end
+
         it 'should return true' do
           runner.stop.should be_true
         end
@@ -151,32 +136,43 @@ describe Guard::RackRunner do
 
       context 'rackup returns unsuccessful exit status' do
         before do
-          Guard::UI.expects(:info).with(regexp_matches(/.+/))
-          kill_stub.returns(1)
+          Guard::UI.stubs(:info)
+          wait_stub.returns([pid, status_stub])
+          status_stub.stubs(:exitstatus).returns(1)
         end
+
         it 'should return false' do
           runner.stop.should be_false
         end
+
         it 'should send some kind of message to UI.info' do
+          Guard::UI.expects(:info).with(regexp_matches(/.+/))
+          runner.stop
+        end
+      end
+
+      context 'kill times out' do
+        before do
+          Guard::UI.stubs(:info)
+          wait_stub.raises(Timeout::Error)
+          Process.expects(:kill).with(9, pid)
+        end
+
+        it 'should return false' do
+          runner.stop.should be_false
+        end
+
+        it 'should send some kind of message to UI.info' do
+          Guard::UI.expects(:info).with(regexp_matches(/.+/))
           runner.stop
         end
       end
     end
 
-    context "pid file does not exist" do
-      it "should return true if there's no pid file" do
+    context "pid does not exist" do
+      it "should return true" do
         runner.stop.should be_true
       end
-    end
-
-  end
-
-  describe '#sleep_time' do
-    let(:timeout) { 30 }
-    let(:options) { default_options.merge(:timeout => timeout) }
-
-    it "should adjust the sleep time as necessary" do
-      runner.sleep_time.should == (timeout.to_f / Guard::RackRunner::MAX_WAIT_COUNT.to_f)
     end
   end
 end
